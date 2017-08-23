ad3d88d1a7df90a3a7819e0431aa5a26b4142a97
gexiangdong <gexiangdong@clschina.com>
2017-08-23 16:35:12 +0800

    update test class

--- security-rest-jersey/src/test/java/cn/devmgr/test/AppTest.java
+++ security-rest-jersey/src/test/java/cn/devmgr/test/AppTest.java
@@ -3,10 +3,14 @@ package cn.devmgr.test;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import cn.devmgr.example.springjersey.Application;
+
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = Application.class, webEnvironment = WebEnvironment.RANDOM_PORT)
 public class AppTest {
 
 	@Test
